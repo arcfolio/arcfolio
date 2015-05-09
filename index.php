e4c1b3c319796c796e20f6d99949aff3c8291bee
joebro12 <joeycuty@gmail.com>
2015-05-08 22:55:32 -0500

    upgrade to Angular
    Connected site to angular.
    Added angular routing to index and ui page. added ng-show to create
    account form.
    "Create account" toggles it.
    Added basic angular Bootstrap Carousel for use in newsfeed.

--- index.php
+++ index.php
@@ -32,22 +32,8 @@
 	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-route.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.8/angular-cookies.js"></script>
     
-    <script type="text/javascript">
-	$(window).load(function() {
-		var jumboHeight = $('.jumbotron').outerHeight();
-		console.log("console works");
-		console.log(jumboHeight);
-		function parallax(){
-			var scrolled = $(window).scrollTop();
-			$('.bg').css('height', (jumboHeight-scrolled) + 'px');
-		}
-		
-		$(window).scroll(function(e){
-			parallax();
-		});
-	});
-		
-	</script>
+	<script src="js/app.js"></script>
+    
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -56,155 +42,13 @@
     <![endif]-->
   </head>
 
-  <body class="mainbody">
+  <body ng-app="Arcfolio" class="mainbody">
   
-  <div class="navbar navbar-inverse navbar-static-top" role="navigation">
-
-	<div class="container">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        </button>
-          	<a href="index.php" class="navbar-brand" style="font-family: oswald;">Arc<span style="color:#4DA2BC">Folio</span></a>
-    </div>
-
-    <div style="" aria-expanded="true" class="navbar-collapse navbar-ex1-collapse collapse in">
-
-        <div class="col-sm-6 col-md-8">
-        <form class="navbar-form" role="search">
-        <div style="width: 100%;" class="input-group">
-            <input style="background-color: rgb(245, 245, 245);" class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-            </div>
-        </div>
-        </form>
-        </div>
-        <ul class="nav navbar-nav pull-right">
-          	<li style="padding-top: 0px; margin-top: 0px;">
-                <a style="padding-top: 12px; padding-right: 0px;" href="http://www.twitter.com">
-          <img src="res/images/twitter.fw.png" style="width: 25px;">
-          		</a>
-            </li>
-            <li style="padding-top: 0px; margin-top: 0px; padding-right: 0px;">
-                <a style="padding-top: 12px;" href="http://www.twitter.com">
-          <img src="res/images/fa.fw.png" style="width: 25px;">
-          		</a>
-            </li>
-            
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-
-
-    </div>
-</div>
-
-</div>
+  <navbar></navbar>
    
-
     
-        	<div class="container">
-            	<div class="row" style="padding-left:10px; padding-right:10px; margin-bottom:15px;">
-                	<div style="background-color: rgba(248, 248, 248, 0.9); background-image:url(res/images/background.fw.png); background-size:cover; background-repeat:no-repeat; box-shadow: 5px 5px 3px rgb(102, 102, 102); text-align: center; height: 150px; display: flex; align-items: center;" class="col-xs-12"><h1 style="font-family: &quot;Oswald&quot;,sans-serif; width: 100%; font-size:50px; margin: 0px;">Arc<span style="color:#4DA2BC">Folio</span>
-                    <small style="margin-top: 0px; margin-bottom: 20px; color:#FFFFFF ">Killer cool slogan to reel folks in.</small></h1></div>
-                </div>
-                
-                
-                
-            	<div class="row" style=" padding-left:10px; padding-right:10px;">
-                            
-                            <div class="col-md-8 col-md-push-4">
-                                
-                                <!--div class="row visible-xs-block visible-sm-block" style="margin-bottom: 10px; padding-right: 10px;">
-                	<div class="col-xs-10" style="background-color: rgb(255, 255, 255); text-indent: 1cm; box-shadow: 5px 5px 3px rgb(102, 102, 102); padding-left:0;padding-right:0;">
-                    
-                                    	 <input style="border-radius: 0px; border: medium none; font-weight: bold; color: rgb(26, 0, 115);" class="form-control" placeholder="Search" required="">
-                                    </div>
-                                    	<div style="padding: 0px; margin: 0px;" class="col-xs-2">
-                                        	<div style="margin-left: 10px; margin-right: -10px;" class="row"><button style="color: #EBEBEB; background-color: rgb(26, 0, 115); font-weight: bold; width: 100%; border: medium none; border-radius: 0px; box-shadow: 5px 5px 3px rgb(102, 102, 102); padding-top: 7px; padding-bottom: 8px; overflow:hidden;" type="button" class="btn btn-primary">Search</button>
-                                            </div>
-                                        </div>
-                </div>
-                
-                <!----------------------------------------------------------------------------------------------------------------->
-                
-                                <!---div class="row visible-lg-block visible-md-block" style=" padding-right: 10px; margin-left:0; margin-bottom:15px;">
-                	<div class="col-xs-10" style="background-color: rgb(255, 255, 255); text-indent: 1cm; box-shadow: 5px 5px 3px rgb(102, 102, 102);padding-left:0;padding-right:0;">
-                                    	 <input style="border-radius: 0px; border: medium none; font-weight: bold; color: rgb(26, 0, 115);" class="form-control" placeholder="Search" required="">
-                                    </div>
-                                    	<div style="padding: 0px; margin: 0px;" class="col-xs-2">
-                                        	<div style="margin-left: 10px; margin-right: -10px;" class="row"><button style="color:  #EBEBEB; background-color: rgb(26, 0, 115); font-weight: bold; width: 100%; border: medium none; border-radius: 0px; box-shadow: 5px 5px 3px rgb(102, 102, 102); padding-top: 7px; padding-bottom: 8px;" type="button" class="btn btn-primary">Search</button>
-                                            </div>
-                                        </div>
-                </div-->
-                				
-                                <div class="row visible-lg-block visible-md-block" style="margin-left: 0;">
-                                    <div class="col-xs-12" style="background-color: rgba(248, 248, 248, 0.9); text-align: center; box-shadow: 5px 5px 3px rgb(102, 102, 102); margin-bottom:15px; height:500px; display:flex; align-items:center;">
-                                       <h3 style="font-family: &quot;ruda&quot;,sans-serif; width:100%; font-size: 48px; color: rgb(26, 0, 115); margin-top: 15px; font-weight: 700;">Some How it works stuff.<small style="font-family: &quot;ruda&quot;,sans-serif; margin-left: 19px; color: rgb(153, 153, 153); font-weight: 700;">Or whatever else.</small></h3>
-                                    </div>
-                                </div>
-                
-                
-                            </div>
-                            
-                            
-                            
-                        <div class="col-md-4 col-md-pull-8">
-                        	<div style="background-color: rgba(248, 248, 248, 0.9); text-align: center; box-shadow: 5px 5px 3px rgb(102, 102, 102); padding-bottom:15px; margin-bottom:15px;" class="row">
-                            	<div class="col-xs-12">
-                                <div class="row">
-                                    <div class="col-xs-12">
-                                       <h3 style="font-family: &quot;ruda&quot;,sans-serif; color: rgb(26, 0, 115); margin-top: 15px; font-weight: 700;">Login.<small style="font-family: &quot;ruda&quot;,sans-serif; margin-left: 19px; color: rgb(153, 153, 153); font-weight: 700;">Create an Account</small></h3>
-                                    </div>
-                                </div>
-                                
-                                <div class="row" style="padding-bottom:15px;">
-                                    <div class="col-xs-12">
-                                        <form action="ui.php" class="form-signin">
-                                            <div style="margin: 10px 0px; text-align: left; text-indent: 9px;">
-                                                <label style="text-align: left;" for="inputEmail">Email address</label>
-                                                <input style="text-align:center;font-weight: bold; color: rgb(26, 0, 115); border: 2px solid rgb(26, 0, 115); border-radius: 4px;" id="inputEmail" class="form-control" placeholder="Email address" required="false" autofocus type="email">
-                                            </div>                                    
-                                            <div style="margin: 10px 0px; text-align: left; text-indent: 9px;">
-                                                <label for="inputPassword">Password</label>
-                                                <input style="text-align:center;font-weight: bold; color: rgb(26, 0, 115); border: 2px solid rgb(26, 0, 115);border-radius: 4px;" id="inputPassword" class="form-control" placeholder="Password" required="" type="password">
-                                             </div>
-                                            <div class="checkbox">
-                                              <label style="font-weight:bold;">
-                                                <input value="remember-me" type="checkbox"> Remember me
-                                              </label>
-                                            </div>
-                                            <button style="background-color:rgb(26, 0, 115); border-radius:4px;" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                                          </form>
-                                    </div>
-                                </div>
-                                </div>
-                                </div>
-                                
-                
-                			
-                                <div class="row visible-xs-block visible-sm-block">
-                                    <div class="col-xs-12" style="background-color: rgba(248, 248, 248, 0.9); text-align: center; box-shadow: 5px 5px 3px rgb(102, 102, 102); padding-bottom:15px; height:500px; display:flex; align-items:center;">
-                                       <h3 style="font-family: &quot;ruda&quot;,sans-serif; width:100%; font-size: 48px; color: rgb(26, 0, 115); margin-top: 15px; font-weight: 700;">Some How it works stuff.<small style="font-family: &quot;ruda&quot;,sans-serif; margin-left: 19px; color: rgb(153, 153, 153); font-weight: 700;">Or whatever else.</small></h3>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                            
-                </div>
-            </div>
+				<div ng-view></div>
+            
             <div class="row" style=" min-height:300px; height:auto; margin:0;">
       		</div>
             <div class="row" style="background-color: rgb(210, 210, 210); margin-top: 10px; margin-left:0 ; margin-right:0; border-top: 2px solid rgb(152, 152, 152); padding-top: 10px;">
---
+++ js/app.js
@@ -0,0 +1,106 @@
+//angular module for Arcfolio.//
+var app = angular.module("Arcfolio", ['ngRoute', 'ui.bootstrap']);
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//configure router - used for sending user to different pages dinamically.//*/
+app.config(function($routeProvider, $locationProvider) 
+{
+	
+	console.log("router working...");
+    $locationProvider.html5Mode(true);
+  	$routeProvider
+	.when('/arcfolio/index.php', 
+	{
+	   	templateUrl: 'html_plugins/default.html',
+	   	controller: 'testController'
+	})
+	
+	
+	.when('/arcfolio/user', 
+		{
+		templateUrl: 'html_plugins/userInterface.html',
+		controller: 'testController2'
+  	})
+	//send all other pages back to index.
+    .otherwise(
+	{
+		redirectTo: '/arcfolio/index.php'
+	});
+			
+});
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+** navbar ** */
+
+app.directive('navbar', function()
+{
+	return	{
+			  restrict: 'E',
+			  templateUrl: 'html_plugins/navBar.html'
+			};
+});
+
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+** newsfeed ** */
+//will need radical rewrite.
+	
+app.directive('newsfeed', function()
+{
+	return	{
+		  		restrict: 'E',
+		  		controller: ['$scope', function($scope)
+				{
+					  $scope.myInterval = 2000;
+					  var slides = $scope.slides = [];
+					  
+					  $scope.addSlide = function() {
+						var newWidth = 600 + slides.length + 1;
+						slides.push({
+						  image: 'http://placekitten.com/' + newWidth + '/300',
+						  text: ['Serious Design work needed.','Angular functionality works tho.','so now its just kittens.','Even though Im a dog person.'][slides.length % 4] + ' ' +
+							['', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+						});
+					  };
+					  for (var i=0; i<4; i++) {
+						$scope.addSlide();
+					  }
+				}],
+				templateUrl: 'html_plugins/newsFeed.html'
+			};
+
+});
+
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+** registerController ** */
+app.controller('registerController', function($scope) {
+	
+	//this hides the re-enter password input in default.html//
+	$scope.registerBool = false;
+	
+	
+	//this toggles the re-enter password input.
+	$scope.register = function() {
+		console.log("clicked");
+		if($scope.registerBool == false)
+		{ $scope.registerBool = true; }
+		else if($scope.registerBool == true)
+		{ $scope.registerBool = false }
+		};
+	
+});
+
+
+//these two are test controllers.
+app.controller('testController', function($scope) {
+	
+	$scope.test = 'hello world';
+	
+});
+
+app.controller('testController2', function($scope) {
+	
+	$scope.test = 'hello world';
+	
+});
